6699dfa9c085fe7f5b013c29c1aee64697bdbf02
SkyStardust <2277425580@qq.com>
2016-09-16 01:54:13 +0800

    初始化插件

---
+++ src/PeerLessGamemodeForPC.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import org.bukkit.plugin.java.*;
+
+public class PeerLessGamemodeForPC extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+		
+	}
+
+	@Override
+	public void onDisable()
+	{
+	
+	}
+	public void setUpListener()
+	{
+		
+	}
+	public void setUpSettingManager()
+	{
+		
+	}
+	public void setUpPlugin()
+	{
+		setUpListener();
+		setUpSettingManager();
+	}
+	
+}
