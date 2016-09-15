9c6674c803bd946fad3d200ce31943857307dc12
SkyStardust <2277425580@qq.com>
2016-09-16 02:20:48 +0800

    添加无法控制通用设置异常

---
+++ src/com/xingyi/c01/PGException/UnableAccessGeneralConfigException.java
@@ -0,0 +1,13 @@
+package com.xingyi.c01.PGException;
+
+public class UnableAccessGeneralConfigException extends Exception
+{
+	public UnableAccessGeneralConfigException()
+	{
+		
+	}
+	public UnableAccessGeneralConfigException(String s)
+	{
+		super(s);
+	}
+}
