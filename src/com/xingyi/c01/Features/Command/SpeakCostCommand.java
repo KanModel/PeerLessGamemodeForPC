5cc4fcddd736e3fc45106fcc34676eea71f55add
Kan_Model <kgdwhsk@gmail.com>
2016-09-19 20:37:44 +0800

    添加聊天扣除饥饿值

--- src/com/xingyi/c01/Features/Command/SpeakCostCommand.java
+++ src/com/xingyi/c01/Features/Command/SpeakCostCommand.java
@@ -1,7 +1,15 @@
 package com.xingyi.c01.Features.Command;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 /**
  * Created by kgdwhsk on 2016/9/19.
  */
-public class SpeakCostCommand {
+public class SpeakCostCommand implements CommandExecutor{
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
 }
